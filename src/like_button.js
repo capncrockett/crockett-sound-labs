26db4766005525263378dfa8b5d75bbfb301aa56
root <root@YOGA-720.localdomain>
2022-01-12 13:54:00 -0800

    Remove double render per Erik's suggestion.

--- src/like_button.js
+++ src/like_button.js
@@ -8,43 +8,43 @@ class LikeButton extends React.Component {
     this.state = { liked: false };
   }
 
-//   render() {
-//     if (this.state.liked) {
-//       return 'You liked this.';
-//     }
-
-//     // Display a "Like" <button>
-//     return (
-//       <button onClick={() => this.setState({ liked: true })}>
-//         Like
-//       </button>
-//     );
-//   }
-// }
-
-// const domContainer = document.querySelector('#like_button_container');
-// ReactDOM.render(e(LikeButton), domContainer);
-
   render() {
     if (this.state.liked) {
-      return 'You liked comment number ' + this.props.commentID;
+      return 'You liked this.';
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    // Display a "Like" <button>
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>
     );
   }
 }
 
-// Find all DOM containers, and render Like buttons into them.
-document.querySelectorAll('.like_button_container')
-  .forEach(domContainer => {
-    // Read the comment ID from a data-* attribute.
-    const commentID = parseInt(domContainer.dataset.commentid, 10);
-    ReactDOM.render(
-      e(LikeButton, { commentID: commentID }),
-      domContainer
-    );
-  });
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
+//   render() {
+//     if (this.state.liked) {
+//       return 'You liked comment number ' + this.props.commentID;
+//     }
+
+//     return e(
+//       'button',
+//       { onClick: () => this.setState({ liked: true }) },
+//       'Like'
+//     );
+//   }
+// }
+
+// // Find all DOM containers, and render Like buttons into them.
+// document.querySelectorAll('.like_button_container')
+//   .forEach(domContainer => {
+//     // Read the comment ID from a data-* attribute.
+//     const commentID = parseInt(domContainer.dataset.commentid, 10);
+//     ReactDOM.render(
+//       e(LikeButton, { commentID: commentID }),
+//       domContainer
+//     );
+//   });
